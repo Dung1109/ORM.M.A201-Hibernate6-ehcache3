fb3bafdf796713b122ce2a7ba32f12b891b4d045
Ngoc Anh <ngocanha85@gmail.com>
2023-04-16 23:29:43 +0700

    second-commit

--- src/test/java/org/hibernate/AllUnitTestSuite.java
+++ src/test/java/org/hibernate/AllUnitTestSuite.java
@@ -3,6 +3,7 @@ package org.hibernate;
 import org.hibernate.movieDAO.MovieDAOImplTest;
 import org.hibernate.movietypeDAO.MovieTypeDAOImplTest;
 import org.hibernate.typeDAO.TypeDAOImplTest;
+import org.hibernate.typeSecondLevelCache.TypeSecondLevelCacheTest;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
@@ -10,7 +11,8 @@ import org.junit.platform.suite.api.Suite;
 @SelectClasses({
         MovieDAOImplTest.class,
         TypeDAOImplTest.class,
-        MovieTypeDAOImplTest.class
+        MovieTypeDAOImplTest.class,
+        TypeSecondLevelCacheTest.class
 })
 public class AllUnitTestSuite {
 }
